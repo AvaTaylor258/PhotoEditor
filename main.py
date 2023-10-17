b2dd805ca6d57c8308cbd45c1de5dcc64e80c4d9
s10015164 <ava.taylor258@gmail.com>
2023-10-17 10:29:32 -0500

    2nd - imported PIL; c+p pointillism() + linked to btn; .webp file don't work :(

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import random
+
+import ImageDraw
+from PIL.Image import Image
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from PIL import Image
 # unable to install PIL due to IDE/C: error (c. very stupid and frustrating pip)
 
 class PhotoEditorApp(App):
@@ -7,9 +12,22 @@ class PhotoEditorApp(App):
 
 class Editor(Screen):
     def change_image(self, t):
-        print(t)
         self.ids.image_display.source = f"user_images/{t}"
-    def black_and_white(self, t, red_val, green_val, blue_val):
+
+    def pointillism(self, t):
         img = Image.open(t)
+        pixels = img.load()
+        canvas = Image.new("RGB", (img.size[0], img.size[1]), "white")
+        for i in range(500000):
+            x = random.randint(0, img.size[0] - 1)
+            y = random.randint(0, img.size[1] - 1)
+            size = random.randint(10, 15)
+            ellipse_box = [(x, y), (x + size, y + size)]
+            draw = ImageDraw.Draw(canvas)
+            draw.ellipse(ellipse_box, fill=(pixels[x, y][0], pixels[x, y][1], pixels[x, y][2]))
+            del draw
+        new_name = f"{t[0:-4]}_pointed.png"
+        canvas.save(new_name)
+        self.ids.image_display.source = new_name
 
 PhotoEditorApp().run()
